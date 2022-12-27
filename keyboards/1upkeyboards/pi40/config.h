0fc9b0856dadbc401d5cf752bc6f2e294de4689b
jake.braswell <jake.braswell@infrasightsoftware.com>
2022-12-27 11:50:55 -0500

    Custom Pi40 keymap/firmware

--- keyboards/1upkeyboards/pi40/config.h
+++ keyboards/1upkeyboards/pi40/config.h
@@ -32,7 +32,7 @@
 #define RGB_MATRIX_LED_COUNT 47
 #define RGBLED_NUM 47
 #    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS  
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGBLIGHT_LIMIT_VAL 150
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
